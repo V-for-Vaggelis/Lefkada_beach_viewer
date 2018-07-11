e6ec0b08d95257af4505aa3bfc63591a08b7daca
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-11 15:53:50 +0300

    fixed coordinates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyMapComponent from './Map.js'
+import MyMapComponent from './components/Map.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyMapComponent isMarkerShown
+        <MyMapComponent isMarkerShown={false}
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCENzVbJtz4UfWe1CN0Em6l5d7IXmNfAM8&libraries=places"
           loadingElement={<section style={{ height: `100%` }} />}
           containerElement={<div id="map" style={{ height: `100vh` }} />}
