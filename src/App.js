478dd0db792e3e01a8a769cf2ed4acf719bad044
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-12 18:23:12 +0300

    Added 5 beach locations on the map

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ class App extends Component {
       center: {lat: 38.7066464, lng: 20.640729999999962},
       zoom: 11
     });
+    let beaches = [{title: 'Kathisma', location: {lat: 38.7767939, lng: 20.600207599999976}},
+    {title: 'Egremni', location: {lat: 38.6374903, lng: 20.558387700000026}},
+    {title: 'Porto-katsiki', location: {lat: 38.6011744, lng: 20.55033739999999}},
+    {title: 'Vasiliki beach', location: {lat: 38.6286429, lng: 20.606477799999993}},
+    {title: 'Agios Nikitas', location: {lat: 38.790081, lng: 20.613406}}
+  ];
+    let markers = [];
+    for (let beach of beaches) {
+      let marker = new window.google.maps.Marker({
+        position: beach.location,
+        map: map,
+        title: beach.title,
+        // Create a drop marker effect
+        animation: window.google.maps.Animation.DROP
+      });
+      markers.push(marker);
+    }
   }
   componentDidMount() {
     this.initMap()
