cfad996ab05ada1cc2595caf19f3275ee473c811
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-16 18:13:28 +0300

    Made it show functional infowindow during filtering

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       zoom: 11
     });
     let markers = [];
-    let photos = [];
+
     let infoWindow = new window.google.maps.InfoWindow();
     for (let beach of beaches) {
       let marker = new window.google.maps.Marker({
@@ -37,51 +37,56 @@ class App extends Component {
       });
       markers.push(marker);
       marker.addListener('click', function(e) {
-        if (infoWindow.marker != marker) {
-          infoWindow.marker = marker;
-          infoWindow.setContent(`<div> ${marker.title} <button id="photos-link"> View photos </button> </div>`);
-          infoWindow.open(map, marker);
-
-          // Make sure the marker property is cleared if the infowindow is closed.
-          document.getElementById("photos-link").addEventListener("click", function() {
-            if (photos.length > 0) {
-              photos = []
-            }
-            let link = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f97455aeea8de971ec02dc9714816d4&lat=${beach.location.lat}&lon=${beach.location.lng}&radius=0.2&radius_units=km&per_page=20&format=json&nojsoncallback=1`
-            fetch(link).then(function(res) {
-              res.json().then(function(parsed) {
-                console.log(parsed)
-                // Create flickr image link method from https://stackoverflow.com/questions/43703296/use-json-output-from-flickr-to-display-images-from-search
-                let _s = parsed.photos.photo;
-                for (let z = 0; z < parsed.photos.photo.length; z++)
-                {
-                  let currentPhotoUrl = 'https://farm'+_s[z]['farm']+'.staticflickr.com/'+_s[z]['server']+'/'+_s[z]['id']+'_'+_s[z]['secret']+'_z.jpg'
-
-                  // console.log(currentPhotoUrl)
-                  let pic = {alt: `A photo of ${beach.title} in Lefkada`, url: currentPhotoUrl}
-                  photos.push(pic)
-                }
-              }).then(() =>
-              this.setState(() => ({
-                pictures: photos,
-                modal: true
-              }))
-            );
-          }.bind(app)
-        ).catch(function(err) {
-          console.log(err)
-        })
+        app.createInfoWindow(marker, infoWindow, map, beach);
       })
-      infoWindow.addListener('closeclick', function() {
-        infoWindow.setMarker = null;
-      });
     }
+    this.setState(() => ({
+      markers: markers,
+      map: map
+    }))
+  }
+
+  createInfoWindow = (marker, infoWindow, map, beach) => {
+    let app = this;
+    let photos = [];
+    if (infoWindow.marker != marker) {
+      infoWindow.marker = marker;
+      infoWindow.setContent(`<div> ${marker.title} <button id="photos-link"> View photos </button> </div>`);
+      infoWindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      document.getElementById("photos-link").addEventListener("click", function() {
+        if (photos.length > 0) {
+          photos = []
+        }
+        let link = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f97455aeea8de971ec02dc9714816d4&lat=${beach.location.lat}&lon=${beach.location.lng}&radius=0.2&radius_units=km&per_page=20&format=json&nojsoncallback=1`
+        fetch(link).then(function(res) {
+          res.json().then(function(parsed) {
+            console.log(parsed)
+            // Create flickr image link method from https://stackoverflow.com/questions/43703296/use-json-output-from-flickr-to-display-images-from-search
+            let _s = parsed.photos.photo;
+            for (let z = 0; z < parsed.photos.photo.length; z++)
+            {
+              let currentPhotoUrl = 'https://farm'+_s[z]['farm']+'.staticflickr.com/'+_s[z]['server']+'/'+_s[z]['id']+'_'+_s[z]['secret']+'_z.jpg'
+
+              // console.log(currentPhotoUrl)
+              let pic = {alt: `A photo of ${marker.title} in Lefkada`, url: currentPhotoUrl}
+              photos.push(pic)
+            }
+          }).then(() =>
+          this.setState(() => ({
+            pictures: photos,
+            modal: true
+          }))
+        );
+      }.bind(app)
+    ).catch(function(err) {
+      console.log(err)
+    })
+  })
+  infoWindow.addListener('closeclick', function() {
+    infoWindow.setMarker = null;
   });
 }
-this.setState(() => ({
-  markers: markers,
-  map: map
-}))
 }
 
 hideModal = () => {
@@ -92,6 +97,8 @@ hideModal = () => {
 
 filterLocation = (locationName) => {
   let markers = this.state.markers
+  let infoWindow = new window.google.maps.InfoWindow();
+  let app = this;
   for (let marker of markers) {
     if (marker.title === locationName) {
       // If location was filtered out, this will show it again
@@ -99,6 +106,9 @@ filterLocation = (locationName) => {
       marker.setAnimation(window.google.maps.Animation.BOUNCE);
       setTimeout(function () {
         marker.setAnimation(null);
+        let beach = beaches.filter((b) => b.title === locationName)
+        console.log(beach)
+        app.createInfoWindow(marker, infoWindow, app.state.map, beach[0]);
       }, 600);
     }
     else {
@@ -106,6 +116,7 @@ filterLocation = (locationName) => {
     }
   }
 }
+
 componentDidMount() {
   this.initMap()
 }
