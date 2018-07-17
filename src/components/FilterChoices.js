d966367f064e4cb77b90644c005b6878df357dfa
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-18 00:41:11 +0300

    Added blue flags on rewarded beaches

--- src/components/FilterChoices.js
+++ src/components/FilterChoices.js
@@ -3,11 +3,20 @@ import '../App.css';
 import { ButtonGroup, Button } from 'react-bootstrap';
 
 function FilterOptions (props)  {
+  let checkFlag = (val) => {
+    if (val.flag) {
+      return (<span id='flag'>&#9873;</span>)
+    }
+    else {
+      return
+    }
+  }
+
   return (
     <ButtonGroup vertical>
     {props.options.map((option) => {
       return (
-        <Button key={option.title} value={option.title} onClick={() => props.applyFilter(option.title)}>{option.title}</Button>
+        <Button key={option.title} value={option.title} onClick={() => props.applyFilter(option.title)}>{option.title} {checkFlag(option)} </Button>
       )
     })}
     </ButtonGroup>
