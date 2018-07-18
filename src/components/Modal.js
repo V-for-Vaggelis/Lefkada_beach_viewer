33271b12c04c55669ee080bdd7e26c7d9625655e
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-18 16:53:21 +0300

    Made modal's content responsive

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,6 +12,7 @@ function ShowModal (props)  {
             <Carousel.Item key={pic}>
               <Image responsive width={"100%"} alt={`A picture of ${props.beach}`} src={pic} />
               <Carousel.Caption id="caption">
+                <h3 id="beach-header">{props.beach} Leukada</h3>
                 <a id="flickr-link" href="https://www.flickr.com/">www.flickr.com</a>
               </Carousel.Caption>
             </Carousel.Item>
