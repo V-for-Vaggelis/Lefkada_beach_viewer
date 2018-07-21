c12e8f9bdd8eda676fa245bd523f586a5a448f99
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-22 00:42:51 +0300

    Made dropdown menu options filter the list of beaches

--- src/components/FilterChoices.js
+++ src/components/FilterChoices.js
@@ -3,6 +3,25 @@ import '../App.css';
 import { ButtonGroup, Button } from 'react-bootstrap';
 
 class FilterOptions extends Component  {
+  state = {
+    filtered: this.props.options
+  }
+
+  // Filters options based on selected option from dropdown
+  filterOptions = (filter) => {
+    console.log(filter)
+    let filtered = this.props.options.filter((option) => {
+      for (let word of option.filters) {
+        if (word === filter) {
+          return true;
+        }
+      }
+    })
+    console.log(filtered);
+    this.setState(() => ({
+      filtered: filtered
+    }))
+  }
 
   // Adds a blue flag to rewarded beaches
   checkFlag = (val) => {
@@ -13,20 +32,21 @@ class FilterOptions extends Component  {
       return
     }
   }
+
   render () {
     return (
       <ButtonGroup id="group" aria-label="Show all locations on map" vertical>
-        <select id="filter-options" defaultValue="All beaches">
-          <option value="All beaches">All beaches</option>
-          <option value="organised">organised</option>
-          <option value="partly-organised">partly-organised</option>
-          <option value="untouched">untouched</option>
-          <option value="camping">camping</option>
-          <option value="water-sports">water-sports</option>
-          <option value="windsurfing">windsurfing</option>
-          <option value="sky-parachute">sky-parachute</option>
+        <select id="filter-options" defaultValue="All beaches" onChange={(e) => this.filterOptions(e.target.value)}>
+          <option className="option" value="all beaches">all beaches</option>
+          <option className="option" value="organised">organised</option>
+          <option className="option" value="partly-organised">partly-organised</option>
+          <option className="option" value="unspoilt">unspoilt</option>
+          <option className="option" value="camping">camping</option>
+          <option className="option" value="water-sports">water-sports</option>
+          <option className="option" value="windsurfing">windsurfing</option>
+          <option className="option" value="sky-parachute">sky-parachute</option>
         </select>
-        {this.props.options.map((option) => {
+        {this.state.filtered.map((option) => {
           return (
             <Button href="#photos-link" aria-label="Filter location on map" className="filter-button" key={option.title}
               value={option.title} onClick={() => this.props.applyFilter(option.title)}>{option.title} {this.checkFlag(option)} </Button>
